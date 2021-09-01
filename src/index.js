8097116f9d7df0cedde28d804c1488b8e2c9780f
joel <joel2000nickson@gmail.com>
2021-09-01 19:31:33 +0530

    initial commit to express app for postgres

---
+++ src/index.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const app = express();
+const dotenv = require("dotenv");
+const { Pool } = require('pg');
+
+dotenv.config({ path: ".env" });
+const url = process.env.DATABASE_URL
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+
+let query = 'SELECT * FROM usertable;';
+const pool = new Pool({connectionString:url, ssl:true})
+
+async function getAllUsers(query) {
+    let response;
+    try {
+        response = await pool.query(query);
+    } catch (error) {
+        throw error;
+    }
+    return response.rows;
+}
+
+async function setUsers(query) {
+    let response;
+    try {
+        await pool.query(query);
+    } catch (error) {
+        throw error;
+    }
+}
+
+app.get('/api', async (req, res) => {
+    query = 'SELECT * FROM usertable;';
+    const users = await getAllUsers(query);
+    console.log(users); // returns all users fine
+    res.status(200).send(users);
+});
+
+app.get('/api/:id', async (req, res) => {
+    const id = req.params.id;
+    query = `SELECT * FROM usertable WHERE id=${id};`;
+    const users = await getAllUsers(query);
+    console.log(users); // returns all users fine
+    res.status(200).send(users);
+});
+
+app.post('/api', async (req, res) => {
+    const {name, pass} = req.body;
+
+    query = `INSERT INTO usertable(name, pass) VALUES( '${name}', '${pass}' );`
+    await setUsers(query);
+    res.sendStatus(200)
+
+});
+
+// app.post('/api/bulk', async (req, res) => {
+//     const {name, pass} = req.body;
+
+//     query = `INSERT INTO usertable(name, pass) VALUES( '${name}', '${pass}' )`
+//     await pool.query(query);
+//     res.sendStatus(200)
+
+// });
+
+
+app.put('/api/:id', async (req, res) => {
+    const id = req.params.id;
+    const {name, pass} = req.body;
+
+    query = `UPDATE usertable 
+            SET name='${name}',pass='${pass}' 
+            WHERE id=${id};`
+    await setUsers(query);
+    res.sendStatus(200)
+
+});
+
+// app.patch('/api/:id', async (req, res) => {
+//     const id = req.params.id;
+//     const {name, pass} = req.body;
+
+//     query = `UPDATE usertable 
+//             SET name='${name}',pass='${pass}' 
+//             WHERE id=${id};`
+//     await pool.query(query);
+//     res.sendStatus(200)
+
+// });
+
+
+app.delete('/api/:id', async (req, res) => {
+    const id = req.params.id;
+    
+    query = `DELETE FROM usertable WHERE id = ${id};`
+    await setUsers(query);
+    res.sendStatus(200)
+
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on  http://localhost:${PORT}`);
+});
